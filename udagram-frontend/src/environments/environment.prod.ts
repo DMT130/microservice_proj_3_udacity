74d986de63366dbc803232e806d9894ad071393a
dmt130 <derciomichaque@gmail.com>
2022-09-18 10:54:11 +0200

    adding reverse proxy url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aacb5afeb03674594a779f0a59102ec3-71100959.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
